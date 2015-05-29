2bba1a8039e95e086d89536942f5b5267f00070a
milon-seip <milon.seip@gmail.com>
2015-05-29 14:58:37 +0600

    Team Project(Product Management System Version 1)

---
+++ ProductManagementApp/UI/MenuUI.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace ProductManagementApp
+{
+    partial class MenuUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.addProductButton = new System.Windows.Forms.Button();
+            this.saleProductButton = new System.Windows.Forms.Button();
+            this.reportProductButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Bright", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(285, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Product Management System";
+            // 
+            // addProductButton
+            // 
+            this.addProductButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addProductButton.Location = new System.Drawing.Point(40, 75);
+            this.addProductButton.Name = "addProductButton";
+            this.addProductButton.Size = new System.Drawing.Size(138, 57);
+            this.addProductButton.TabIndex = 1;
+            this.addProductButton.Text = "Add Product";
+            this.addProductButton.UseVisualStyleBackColor = true;
+            this.addProductButton.Click += new System.EventHandler(this.addProductButton_Click);
+            // 
+            // saleProductButton
+            // 
+            this.saleProductButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.saleProductButton.Location = new System.Drawing.Point(248, 75);
+            this.saleProductButton.Name = "saleProductButton";
+            this.saleProductButton.Size = new System.Drawing.Size(138, 57);
+            this.saleProductButton.TabIndex = 1;
+            this.saleProductButton.Text = "Sale Product";
+            this.saleProductButton.UseVisualStyleBackColor = true;
+            this.saleProductButton.Click += new System.EventHandler(this.saleProductButton_Click);
+            // 
+            // reportProductButton
+            // 
+            this.reportProductButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reportProductButton.Location = new System.Drawing.Point(141, 157);
+            this.reportProductButton.Name = "reportProductButton";
+            this.reportProductButton.Size = new System.Drawing.Size(150, 62);
+            this.reportProductButton.TabIndex = 2;
+            this.reportProductButton.Text = "Report Product";
+            this.reportProductButton.UseVisualStyleBackColor = true;
+            this.reportProductButton.Click += new System.EventHandler(this.reportProductButton_Click);
+            // 
+            // MenuUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(438, 256);
+            this.Controls.Add(this.reportProductButton);
+            this.Controls.Add(this.saleProductButton);
+            this.Controls.Add(this.addProductButton);
+            this.Controls.Add(this.label1);
+            this.Name = "MenuUI";
+            this.Text = "Product Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addProductButton;
+        private System.Windows.Forms.Button saleProductButton;
+        private System.Windows.Forms.Button reportProductButton;
+    }
+}
+
---
+++ ProductManagementApp/UI/MenuUI.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ProductManagementApp.UI;
+
+namespace ProductManagementApp
+{
+    public partial class MenuUI : Form
+    {
+        public MenuUI()
+        {
+            InitializeComponent();
+        }
+
+        private void addProductButton_Click(object sender, EventArgs e)
+        {
+            ProductEntryUI productEntryUi = new ProductEntryUI();
+            productEntryUi.Show();
+        }
+
+        private void saleProductButton_Click(object sender, EventArgs e)
+        {
+            ProductSaleUI productSaleUi = new ProductSaleUI();
+            productSaleUi.Show();
+        }
+
+        private void reportProductButton_Click(object sender, EventArgs e)
+        {
+            ProductReportUI productReportUi = new ProductReportUI();
+            productReportUi.Show();
+        }
+    }
+}
